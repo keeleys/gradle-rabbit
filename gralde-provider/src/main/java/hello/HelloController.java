20f3b9115ceb25bb26a7426abcd1477a42e91035
keeley <keeley.jun@qq.com>
2016-06-24 10:44:24 +0800

    Initial commit

---
+++ gralde-provider/src/main/java/hello/HelloController.java
@@ -0,0 +1,19 @@
+package hello;
+
+/**
+ * @user tianjun
+ * @date 16/6/23
+ */
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
